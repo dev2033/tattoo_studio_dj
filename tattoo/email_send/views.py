f413ca07e23c807a8e554ea961dbf52e3773b7e3
dev2033 <dev.elop.02@mail.ru>
2021-05-08 22:55:22 +0300

    refactoring

--- tattoo/email_send/views.py
+++ tattoo/email_send/views.py
@@ -1,6 +1,6 @@
 from django.db import transaction
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views import View
 from django.views.generic import CreateView
 
--- tattoo/tattoo/loguru_logger.py
+++ tattoo/tattoo/loguru_logger.py
@@ -3,7 +3,7 @@ from loguru import logger
 
 
 logger.add(
-    "tattoo/logging/log.json",
+    "logging/log.json",
     format="{time} {level} {message}",
     level="DEBUG",
     rotation="2 MB",
